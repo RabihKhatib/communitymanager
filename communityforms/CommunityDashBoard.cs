23eca0465ad01c6174d97093ab2d29ba977b5697
RabihKhatib <rabih.khatib@gmail.com>
2018-10-29 10:02:46 +0200

    #DN testing git

--- communityforms/CommunityDashBoard.cs
+++ communityforms/CommunityDashBoard.cs
@@ -14,7 +14,7 @@ namespace communityforms
 
             LoadCitizenList();
         }
-
+         
         private void LoadCitizenList()
         {
             citizens = SqliteDAO.loadCitizens();
